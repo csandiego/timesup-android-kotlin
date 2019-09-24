90ff2188fc8fdf5c727fcafc163dfe6c9201db0b
Christopher San Diego <csandiego@gmail.com>
2019-09-24 14:18:33 +0800

    Remove database static singleton accessor

--- app/src/main/java/com/github/csandiego/timesup/room/TimesUpDatabase.kt
+++ app/src/main/java/com/github/csandiego/timesup/room/TimesUpDatabase.kt
@@ -1,8 +1,6 @@
 package com.github.csandiego.timesup.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.github.csandiego.timesup.data.Preset
 
@@ -10,22 +8,4 @@ import com.github.csandiego.timesup.data.Preset
 abstract class TimesUpDatabase : RoomDatabase() {
 
     abstract fun presetDao(): PresetDao
-
-    companion object {
-        private var instance: TimesUpDatabase? = null
-
-        fun getInstance(context: Context): TimesUpDatabase {
-            val currentInstance = instance
-            if (currentInstance != null)
-                return currentInstance
-
-            return synchronized(this) {
-                Room.databaseBuilder(context, TimesUpDatabase::class.java, "TimesUp")
-                    .build()
-                    .also {
-                        instance = it
-                    }
-            }
-        }
-    }
 }
