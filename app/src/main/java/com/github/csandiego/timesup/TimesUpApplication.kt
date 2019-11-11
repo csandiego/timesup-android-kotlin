ba0c718c9f349f0f19d1586a5f43c5d973ce2ebe
Christopher San Diego <csandiego@gmail.com>
2019-11-11 14:01:57 +0800

    Lower minSdkVersion to 14

--- app/src/main/java/com/github/csandiego/timesup/TimesUpApplication.kt
+++ app/src/main/java/com/github/csandiego/timesup/TimesUpApplication.kt
@@ -4,6 +4,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import androidx.multidex.MultiDex
 import com.github.csandiego.timesup.dagger.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -36,5 +37,10 @@ open class TimesUpApplication : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.factory().create(this)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
